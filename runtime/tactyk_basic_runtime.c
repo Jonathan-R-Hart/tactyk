217e6ceb50c619034d2cdd49ad789f82cbdfe967
Jonathan-R-Hart <129913902+Jonathan-R-Hart@users.noreply.github.com>
2023-06-06 07:15:56 -0400

    Begin development of a general-purpose standalone runtime

---
+++ runtime/tactyk_basic_runtime.c
@@ -0,0 +1,56 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include "tactyk.h"
+#include "tactyk_visa.h"
+#include "tactyk_emit.h"
+#include "tactyk_asmvm.h"
+#include "tactyk_emit_svc.h"
+#include "tactyk_pl.h"
+#include "tactyk_debug.h"
+#include "aux_printit.h"
+#include "aux_util.h"
+
+int main(int argc, char *argv[], char *envp[]) {
+    tactyk_init();
+
+    printf("%s\n", TACTYK_SE__DESCRIPTION);
+
+    char *visa_directory_name = "rsc";
+    char *visa_fname = NULL;
+
+    tactyk_visa__init(visa_directory_name);
+    if (visa_fname == NULL) {
+        tactyk_visa__load_config_module("tactyk_core.visa");
+        tactyk_visa__load_config_module("tactyk_core_typespec.visa");
+        tactyk_visa__load_config_module("tactyk_core_ccall.visa");
+        tactyk_visa__load_config_module("tactyk_core_memory.visa");
+        tactyk_visa__load_config_module("tactyk_core_bulk_transfer.visa");
+        tactyk_visa__load_config_module("tactyk_core_stash.visa");
+        tactyk_visa__load_config_module("tactyk_core_tvmcall.visa");
+        tactyk_visa__load_config_module("tactyk_core_xmm_fpmath.visa");
+        tactyk_visa__load_config_module("tactyk_core_math.visa");
+        tactyk_visa__load_config_module("tactyk_core_simd.visa");
+        tactyk_visa__load_config_module("tactyk_core_simd-util.visa");
+    }
+    else {
+        tactyk_visa__load_config_module(visa_fname);
+    }
+    struct tactyk_emit__Context *emitctx = tactyk_emit__init();
+    
+    tactyk_visa__init_emit(emitctx);
+    struct tactyk_asmvm__VM *vm = tactyk_asmvm__new_vm();
+    struct tactyk_asmvm__Context *ctx = tactyk_asmvm__new_context(vm);
+
+    tactyk_debug__configure_api(emitctx);
+    tactyk_emit_svc__configure(emitctx);
+    aux_printit__configure(emitctx);
+    aux_util__configure(emitctx);
+
+    struct tactyk_pl__Context *plctx = tactyk_pl__new(emitctx);
+    tactyk_pl__define_constants(plctx, ".VISA", emitctx->visa_token_constants);
+    
+    printf("nothing to see here.\n");
+    
+    exit(0);
+}
