ccd766ac1435f94054faa18ecf8be01f6abc88e6
Jonathan-R-Hart <129913902+Jonathan-R-Hart@users.noreply.github.com>
2023-12-06 19:31:23 -0500

    fix asm warning

--- rsc/tactyk_asmvm_header.asm
+++ rsc/tactyk_asmvm_header.asm
@@ -527,7 +527,7 @@
     struc rbtactyk
         qwords prog,lwcsi,mcsi,tempa,tempc,tempd,addr1,addr2,addr3,addr4,a,b,c,d,e,f
         owords xa,xb,xc,xd, xe,xf,xg,xh, xi,xj,xk,xl, xm,xn,xo,xp
-        qwords, fs,gs
+        qwords fs,gs
         dwords mxcsr
         unused:  resd 27
     endstruc
@@ -535,7 +535,7 @@
     struc rbnative
         qwords rax,rbx,rcx,rdx, rbp,rsp, rsi,rdi, r8,r9,r10,r11,r12,r13,r14,r15
         owords xmm0,xmm1,xmm2,xmm3, xmm4,xmm5,xmm6,xmm7, xmm8,xmm9,xmm10,xmm11, xmm12,xmm13,xmm14,xmm15
-        qwords, fs,gs
+        qwords fs,gs
         dwords mxcsr
         unused:  resd 27
     endstruc
